c966118eff38fafd44633b89583df6590d4bf7e5
Tawqir Sohail <Tawqir.Sohail@asos.com>
2017-01-09 13:54:47 +0000

    Added all space consuming solutions for finding unique string that I could think of.

--- StringAndArrayExcercises/Strings/DoesTheStringHaveAllUniqueCharacters.cs
+++ StringAndArrayExcercises/Strings/DoesTheStringHaveAllUniqueCharacters.cs
@@ -1,12 +1,101 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Strings
 {
-    class DoesTheStringHaveAllUniqueCharacters
+    /*
+     * Given an arbitrary string, write code that can figure out if
+     * a string has only unique characters.
+     * 
+     * What if no additional space can be used?
+     */
+
+    public class DoesTheStringHaveAllUniqueCharacters
     {
+        /**
+         * @ CheckExhaustively(string => bool)
+         * 
+         * Easiest way to do this imo is to do a brute force search
+         * 
+         * (+) No additional space
+         * (-) O(n^2) worst-case
+         */
+
+        public bool CheckExhaustively(string input)
+        {
+            int length = input.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    if (i != j && input[i] == input[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        /**
+         * @ CheckUsingHashTable(string => bool)
+         * 
+         * Could simply use a HashTable to lookup already seen characters in the string.
+         * 
+         * (+) Efficient, hashtables have a constant lookup time
+         * (-) SPACE complexity of O(n)
+         */
+
+        public bool CheckUsingHashTable(string input)
+        {
+            var length = input.Length;
+            var lookupTable = new Hashtable();
+
+            for (int i = 0; i < length; i++)
+            {
+                if (!lookupTable.ContainsKey(input[i]))
+                {
+                    lookupTable.Add(input[i], 0);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /**
+         * @ CheckUsingBitVector(string => bool)
+         * 
+         * Uses a bit vector which requires 8x|16x less space than hash table.
+         * 
+         * (+) More space efficient compared to Hashtable
+         * (-) Still requires extra space 
+         */
+        public bool CheckUsingBitVector(string input)
+        {
+            var bitArray = new BitArray(char.MaxValue);
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (bitArray[input[i]])
+                {
+                    return false;
+                }
+                bitArray[input[i]] = true;
+            }
+
+            return true;
+        }
+
+        
     }
 }
--- StringAndArrayExcercises/StringsShould/DoesTheStringHaveAllUniqueCharactersShould.cs
+++ StringAndArrayExcercises/StringsShould/DoesTheStringHaveAllUniqueCharactersShould.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using Strings;
 
-namespace StringsShould
+namespace StringsTests
 {
-    class DoesTheStringHaveAllUniqueCharactersShould
+    [TestFixture]
+    public class DoesTheStringHaveAllUniqueCharactersShould
     {
+        private DoesTheStringHaveAllUniqueCharacters _uniquityChecker = new DoesTheStringHaveAllUniqueCharacters();
+
+        [TestCase("abcde", true)]
+        [TestCase("abcdedf", false)]
+        public void CorrectlyCompleteCheckByExhaustiveSearch(string input, bool expectedOutput)
+        {
+            bool actualOutput = _uniquityChecker.CheckExhaustively(input);
+            (actualOutput == expectedOutput).Should().BeTrue();
+        }
+
+        [TestCase("abcd", true)]
+        [TestCase("afera", false)]
+        public void CorrectlyCompleteCheckByHashtableLookUp(string input, bool expectedOutput)
+        {
+            bool actualOutput = _uniquityChecker.CheckUsingHashTable(input);
+            (actualOutput == expectedOutput).Should().BeTrue();
+        }
+
+        [TestCase("abscfetk", true)]
+        [TestCase("asdfasdf", false)]
+        [TestCase("asdofasdfasdfasdfasdfioasdfioawuofiasdfasdfasdiomfiovnasdifasdijfasidfdnjcnas", false)]
+        [TestCase("abcdefghijkl325';]", true)]
+        [TestCase("[]#'/.,;*9*", false)]
+        public void CorrectlyCompleteCheckByBitVector(string input, bool expectedOutput)
+        {
+            bool actualOutput = _uniquityChecker.CheckUsingBitVector(input);
+            (actualOutput == expectedOutput).Should().BeTrue();
+        }
     }
 }
