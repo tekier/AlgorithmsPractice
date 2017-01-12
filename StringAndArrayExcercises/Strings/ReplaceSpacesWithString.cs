1fc5c66e3cb3d362c74a4d466538f4011f87fc9f
Tawqir Sohail <Tawqir.Sohail@asos.com>
2017-01-12 10:54:58 +0000

    Finished ReplaceSpacesWithString. Also installed crunchy NCrunch.

---
+++ StringAndArrayExcercises/Strings/ReplaceSpacesWithString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    public class ReplaceSpacesWithString
+    {
+        public void ReplaceUsingStringBuilder(ref string input)
+        { 
+            StringBuilder builder = new StringBuilder(input);
+            builder.Replace(" ", "_");
+            input = builder.ToString();
+        }
+
+        public void ReplaceUsingBasicStringOps(ref string input)
+        {
+            string temp = string.Empty;
+            foreach (char character in input)
+            {
+                if (character.ToString().Equals(" "))
+                {
+                    temp = temp + "_";
+                }
+                else
+                {
+                    temp =  temp + character;
+                }
+            }
+            input = temp;
+        }
+    }
+}
---
+++ StringAndArrayExcercises/StringsShould/ReplaceSpacesWithStringShould.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using Strings;
+
+namespace StringsTests
+{
+    [TestFixture]
+    public class ReplaceSpacesWithStringShould
+    {
+        private ReplaceSpacesWithString _replacer = new ReplaceSpacesWithString();
+
+        [TestCase("example string", "example_string")]
+        public void ReplaceCorrectlyWhenUsingStringbuilder(string withSpaces, string expectedOutputString)
+        {
+           _replacer.ReplaceUsingStringBuilder(ref withSpaces);
+            withSpaces.Should().Be(expectedOutputString);
+        }
+
+        [TestCase("example string", "example_string")]
+        public void ReplaceCorrectlyWhenUsingBasicArrayOperations(string withSpaces, string expectedOutputString)
+        {
+           _replacer.ReplaceUsingBasicStringOps(ref withSpaces);
+            withSpaces.Should().Be(expectedOutputString);
+        }
+            
+    }
+}
