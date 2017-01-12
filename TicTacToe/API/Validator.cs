fb3100252ed3a625393732752ab4eba4caf18480
Tawqir Sohail <Tawqir.Sohail@asos.com>
2017-01-12 15:17:28 +0000

    Initial commit

---
+++ TicTacToe/API/Validator.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using static System.Configuration.ConfigurationManager;
+
+namespace API
+{
+    public class Validator
+    {
+        private static readonly int CorrectNumberOfInputElements = short.Parse(AppSettings["number of input elements"]);
+        private static readonly int MaxRangeOfInputElements = short.Parse(AppSettings["max range"]);
+
+        public bool ValidateInput(string userInput)
+        {
+            var userInputWithWhitespacesRemoved = userInput.Replace(" ", string.Empty);
+            return IsCorrectLength(userInputWithWhitespacesRemoved) &&
+                   FirstTwoEntriesAreCastableAsIntegers(userInputWithWhitespacesRemoved) &&
+                   LastElementOfUserInputIsXorO(userInputWithWhitespacesRemoved) &&
+                   FirstTwoEntriesAreInRange(userInputWithWhitespacesRemoved);
+        }
+        private bool FirstTwoEntriesAreInRange(string userInputWithWhitespacesRemoved)
+        {
+            var firstElementLessThanMaxValue = short.Parse(userInputWithWhitespacesRemoved[0].ToString()) <=
+                                               MaxRangeOfInputElements;
+            var secondElementLessThanMaxValue = short.Parse(userInputWithWhitespacesRemoved[1].ToString()) <=
+                                                MaxRangeOfInputElements;
+            return firstElementLessThanMaxValue && secondElementLessThanMaxValue;
+        }
+        private bool LastElementOfUserInputIsXorO(string userInputWithWhitespacesRemoved)
+        {
+            var lastElement = userInputWithWhitespacesRemoved.Last().ToString();
+            return lastElement.Equals("X") || lastElement.Equals("O");
+        }
+        private bool FirstTwoEntriesAreCastableAsIntegers(string userInputWithWhitespacesRemoved)
+        {
+            var firstElementIsNumber = char.IsDigit(userInputWithWhitespacesRemoved[0]);
+            var secondElementIsNumber = char.IsDigit(userInputWithWhitespacesRemoved[1]);
+            return firstElementIsNumber && secondElementIsNumber;
+        }
+        private bool IsCorrectLength(string userInputWithNoWhitespace)
+        {
+            return userInputWithNoWhitespace.Length == CorrectNumberOfInputElements;
+        }
+    }
+}
---
+++ TicTacToe/Tests/ValidatorShould.cs
@@ -0,0 +1,39 @@
+﻿using API;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ValidatorShould
+    {
+        private Validator _validatorObject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _validatorObject = new Validator();
+        }
+
+        [TestCase("0 0 X")]
+        [TestCase("1 1 X")]
+        [TestCase("2 2 O")]
+        public void ValidateCorrectlyFormattedUserInputs(string userInput)
+        {
+            bool isValid = _validatorObject.ValidateInput(userInput);
+            isValid.Should().BeTrue();
+        }
+
+        [TestCase("red")]
+        [TestCase("asdflj")]
+        [TestCase("8 8 9")]
+        [TestCase("5 5 O")]
+        public void InvalidateIncorrectlyFormattedUserInputs(string userInput)
+        {
+            bool isValid = _validatorObject.ValidateInput(userInput);
+            isValid.Should().BeFalse();
+        }
+
+    }
+
+}
